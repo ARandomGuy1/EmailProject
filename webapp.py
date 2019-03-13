6ffa75513d5b01e657b3fb607f1b34410e6b2024
ARandomGuy1 <42652957+ARandomGuy1@users.noreply.github.com>
2019-03-13 12:07:02 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,11 @@
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask import render_template
 
+import pprint
+import os
+
+app = Flask(__name__)
+
+app.debug = False
+
+app.secret_key = os.environ['SECRET_KEY'] 
